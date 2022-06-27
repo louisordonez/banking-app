04f1d4f830647eaa1846d302bd3334089854c56f
louisordonez <72607877+louisordonez@users.noreply.github.com>
2022-06-27 13:57:08 +0800

    Add primary button styles

--- src/App.js
+++ src/App.js
@@ -1,5 +1,12 @@
 import './App.css';
+import Button from './components/Button/Button.js';
 
-function App() {}
+function App() {
+  return (
+    <div>
+      <Button variant="btn-primary" text="Confirm" />
+    </div>
+  );
+}
 
 export default App;
--- src/components/Button/Button.js
+++ src/components/Button/Button.js
@@ -1,3 +1,5 @@
+import './Button.css';
+
 export default function Button({ variant = 'btn-primary', text }) {
   return <button className={`btn ${variant}`}>{text}</button>;
 }
