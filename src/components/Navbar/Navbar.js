d4beaeaa37c0513dbe05e0433081dd51a06fea95
grenzk <74033991+grenzk@users.noreply.github.com>
2022-06-28 11:24:22 +0800

    Create nav bar

---
+++ src/components/Navbar/Navbar.js
@@ -0,0 +1,41 @@
+import React, { useState } from 'react';
+import { Link } from 'react-router-dom';
+import * as BoxIcons from 'react-icons/bi';
+import { SidebarData } from './SidebarData';
+
+function Navbar() {
+  const [sidebar, setSidebar] = useState(false);
+
+  const showSidebar = () => setSidebar(!sidebar);
+
+  return (
+    <>
+      <div className="navbar">
+        <Link to="#" className="menu-bars">
+          <BoxIcons.BiMenu onClick={showSidebar} />
+        </Link>
+      </div>
+      <nav className={sidebar ? 'nav-menu active' : 'nav-menu'}>
+        <ul className="nav-menu-items" onClick={showSidebar}>
+          <li className="navbar-toggle">
+            <Link to="#" className="menu-bars">
+              <BoxIcons.BiX />
+            </Link>
+          </li>
+          {SidebarData.map((item, index) => {
+            return (
+              <li key={index} className={item.cName}>
+                <Link to={item.path}>
+                  {item.icon}
+                  <span>{item.title}</span>
+                </Link>
+              </li>
+            );
+          })}
+        </ul>
+      </nav>
+    </>
+  );
+}
+
+export default Navbar;
--- src/index.js
+++ src/index.js
@@ -1,22 +1,12 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import { BrowserRouter, Routes, Route } from 'react-router-dom';
 import App from './App';
-import Dashboard from './Routes/Dashboard';
-import Users from './Routes/Users';
-import Transactions from './Routes/Transactions';
-import Settings from './Routes/Settings';
+
 import './Assets/Styles/Scss/styles.scss';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
-  <BrowserRouter>
-    <Routes>
-      <Route path="/" element={<App />} />
-      <Route path="Dashboard" element={<Dashboard />} />
-      <Route path="Users" element={<Users />} />
-      <Route path="Transactions" element={<Transactions />} />
-      <Route path="Settings" element={<Settings />} />
-    </Routes>
-  </BrowserRouter>
+  <React.StrictMode>
+    <App />
+  </React.StrictMode>
 );
