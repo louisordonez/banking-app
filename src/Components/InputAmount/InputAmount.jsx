6e00cfb730e5927a0f38062a4c8fd6b0980922b7
louisordonez <72607877+louisordonez@users.noreply.github.com>
2022-06-28 10:27:16 +0800

    Refactor input amount design

--- src/Components/InputAmount/InputAmount.jsx
+++ src/Components/InputAmount/InputAmount.jsx
@@ -2,14 +2,12 @@ import React from 'react';
 
 const InputText = () => {
   return (
-    <div className='input-amount-container'>
+    <div className='input-container'>
       <div>
-        <div>
           {`Enter amount`}
           <div className='amount-flex-container'>
-          <span>₱</span><input></input>
+            <span>₱</span><input></input>
           </div>
-        </div>
       </div>
     </div>
   );
