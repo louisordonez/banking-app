424ec554265a03c0f05c3b2da9d9bac2fdd90225
louisordonez <72607877+louisordonez@users.noreply.github.com>
2022-06-30 16:42:09 +0800

    Restore sidebardata js

---
+++ src/Components/Navbar/SidebarData.js
@@ -0,0 +1,29 @@
+import React from 'react';
+import * as BoxIcons from 'react-icons/bi';
+
+export const SidebarData = [
+  {
+    title: 'Dashboard',
+    path: '/',
+    icon: <BoxIcons.BiCategoryAlt />,
+    cName: 'nav-text',
+  },
+  {
+    title: 'Users',
+    path: 'users',
+    icon: <BoxIcons.BiUser />,
+    cName: 'nav-text',
+  },
+  {
+    title: 'Transactions',
+    path: 'transactions',
+    icon: <BoxIcons.BiMoney />,
+    cName: 'nav-text',
+  },
+  {
+    title: 'Settings',
+    path: 'settings',
+    icon: <BoxIcons.BiCog />,
+    cName: 'nav-text',
+  },
+];
