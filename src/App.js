68131d97c403aeeb3200d9046aac4fac39b42b10
louisordonez <72607877+louisordonez@users.noreply.github.com>
2022-07-03 21:54:54 +0800

    Update App.js

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import React, { useEffect, useRef, useState } from 'react';
+import React, { useEffect, useState } from 'react';
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import Login from './Routes/Login/Login';
 import Navbar from './Components/Navbar/Navbar';
