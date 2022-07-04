1a22ee2000b9472914b7aa8f0fae9a5114b7d774
louisordonez <72607877+louisordonez@users.noreply.github.com>
2022-07-04 20:42:41 +0800

    Add admin dashboard components

--- src/Routes/Dashboard/Dashboard.jsx
+++ src/Routes/Dashboard/Dashboard.jsx
@@ -1,10 +1,39 @@
 import React from 'react';
+import * as BoxIcons from 'react-icons/bi';
 
 const Dashboard = () => {
   return (
-    <main>
-      <h2 className='page-header'>Dashboard</h2>
-    </main>
+    <>
+      <h2 className="page-header">Dashboard</h2>
+      <div className="align-items-center page-header">
+        <div className="dashboard-box green-box">
+          <div className="box-header">
+            <div className="align-items-center">
+              <BoxIcons.BiUser size={32} />
+              <span style={{ fontSize: '18px', marginLeft: '12px' }}>
+                Total Users
+              </span>
+            </div>
+          </div>
+          <div className="box-text">
+            <span>100</span>
+          </div>
+        </div>
+        <div className="dashboard-box green-box">
+          <div className="box-header">
+            <div className="align-items-center">
+              <BoxIcons.BiMoney size={32} />
+              <span style={{ fontSize: '18px', marginLeft: '12px' }}>
+                Total Balance
+              </span>
+            </div>
+          </div>
+          <div className="box-text">
+            <span>100.00</span>
+          </div>
+        </div>
+      </div>
+    </>
   );
 };
 
