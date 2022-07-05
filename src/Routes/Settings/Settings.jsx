7bb65796345d82fc25ceaf0b023c0c58c3c2c6d2
louisordonez <72607877+louisordonez@users.noreply.github.com>
2022-07-06 05:19:42 +0800

    Add settings form

--- src/Routes/Settings/Settings.jsx
+++ src/Routes/Settings/Settings.jsx
@@ -37,6 +37,88 @@ const Settings = () => {
     <main>
       <h2 className="page-header">Settings</h2>
       {displayAlert()}
+      <div className="center">
+        <div className="settings center">
+          <div className="modal-header">
+            <div>
+              <span>Edit User</span>
+            </div>
+          </div>
+          <div className="modal-body">
+            <form>
+              <div className="create-user-flex">
+                <div>
+                  <input
+                    className="user-input-text"
+                    placeholder="Account Number"
+                    type="number"
+                    disabled
+                  />
+                </div>
+              </div>
+              <div className="create-user-flex">
+                <div>
+                  <input
+                    className="user-input-text"
+                    placeholder="First Name"
+                    type="text"
+                    required
+                  />
+                </div>
+                <div>
+                  <input
+                    className="user-input-text"
+                    placeholder="Last Name"
+                    type="text"
+                    required
+                  />
+                </div>
+              </div>
+              <div className="create-user-flex">
+                <div>
+                  <input
+                    className="user-input-text"
+                    placeholder="Birthdate"
+                    type="date"
+                    required
+                  />
+                </div>
+                <div>
+                  <select className="user-input-text" required>
+                    <option value="Male">Male</option>
+                    <option value="Female">Female</option>
+                  </select>
+                </div>
+              </div>
+              <div className="create-user-flex">
+                <div>
+                  <input
+                    className="user-input-text"
+                    placeholder="Email"
+                    type="email"
+                    required
+                  />
+                </div>
+                <div>
+                  <input
+                    className="user-input-text"
+                    placeholder="Password"
+                    type="password"
+                    required
+                  />
+                </div>
+              </div>
+              <div className="modal-footer">
+                <input
+                  className="btn-small btn-primary"
+                  type="submit"
+                  value="Submit"
+                />
+              </div>
+            </form>
+          </div>
+        </div>
+      </div>
     </main>
   );
 };
