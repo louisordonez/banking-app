6ccd693d8616ec0fddd010aa28f2ef6ec337defa
louisordonez <72607877+louisordonez@users.noreply.github.com>
2022-07-06 05:01:24 +0800

    Add alert in settings

--- src/Routes/Settings/Settings.jsx
+++ src/Routes/Settings/Settings.jsx
@@ -1,9 +1,42 @@
-import React from 'react';
+import React, { useState } from 'react';
+import Alert from '../../Components/Alert/Alert';
 
 const Settings = () => {
+  const [showAlert, setShowAlert] = useState('none');
+  const [alertType, setAlertType] = useState('');
+  const [alertHeader, setAlertHeader] = useState('');
+  const [alertMessage, setAlertMessage] = useState('');
+
+  const handleCloseAlert = () => {
+    setShowAlert('none');
+  };
+
+  const handleAlert = (alertType, alertHeader, alertMessage) => {
+    setAlertType(alertType);
+    setAlertHeader(alertHeader);
+    setAlertMessage(alertMessage);
+    setShowAlert('block');
+    setTimeout(handleCloseAlert, 5000);
+  };
+
+  const displayAlert = () => {
+    if (showAlert === 'block') {
+      return (
+        <Alert
+          alertType={alertType}
+          alertHeader={alertHeader}
+          alertText={alertMessage}
+          showAlert={showAlert}
+          onClick={handleCloseAlert}
+        />
+      );
+    }
+  };
+
   return (
     <main>
       <h2 className="page-header">Settings</h2>
+      {displayAlert()}
     </main>
   );
 };
