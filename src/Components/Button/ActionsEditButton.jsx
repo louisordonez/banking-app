bc2b87b0b45fef6078e4ed75aa050ed33db537f7
louisordonez <72607877+louisordonez@users.noreply.github.com>
2022-07-06 09:21:57 +0800

    Update btn colors

--- src/Components/Button/ActionsEditButton.jsx
+++ src/Components/Button/ActionsEditButton.jsx
@@ -4,7 +4,7 @@ import * as BoxIcons from 'react-icons/bi';
 const ActionsEditButton = ({ onClick }) => {
   return (
     <button title="Edit" onClick={onClick}>
-      <BoxIcons.BiPencil size={16} style={{ color: '#FCE37E' }} />
+      <BoxIcons.BiPencil size={16} style={{ color: '#f5a623' }} />
     </button>
   );
 };
